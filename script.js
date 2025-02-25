2bf9be32f9856f3c39f578c7defedcb2ddc5a842
Mohamed Azarudeen <mailforazarudeen@gmail.com>
2025-02-25 20:58:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 const addTaskBtn = document.getElementById("addTask");
 const filterBtn = document.querySelector(".filter-options button");
+
+addTaskBtn.addEventListener("click", addTask);
+taskInput.addEventListener("keypress", (e) {
+    if(e.key === "Enter") addTask();
+});
+
+function addTask() {
+    const taskText = taskInput.ariaValueMax.trim();
+    if(taskText === "") return;
+
+    const task = {text: taskText, complted:false};
+    saveTask(task);
+    renderTask(task);
+    taskInput.value = "";
+}
