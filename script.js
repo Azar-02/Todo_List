7dcf55115083e4b5c31eb5384773bca6b5fd0c75
Mohamed Azarudeen <mailforazarudeen@gmail.com>
2025-02-25 20:54:49 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", loadTasks);
+
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+const addTaskBtn = document.getElementById("addTask");
+const filterBtn = document.querySelector(".filter-options button");
